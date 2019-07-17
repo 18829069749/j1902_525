375cac3294a761be395a84af8ceed5aa0d035c9a
18829069749 <1658343900@qq.com>
2019-07-17 09:26:45 +0800

    mapper

---
+++ src/main/java/com/qf/j1902/mapper/PersonMapper.java
@@ -0,0 +1,10 @@
+package com.qf.j1902.mapper;
+
+import com.qf.j1902.vo.Person;
+
+/**
+ * Created by Administrator on 2019/7/17.
+ */
+public interface PersonMapper {
+    public Person findByName();
+}
