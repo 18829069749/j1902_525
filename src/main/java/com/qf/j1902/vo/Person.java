fe2ec4c3cd8215ef39b4bce421562208af5c0f65
18829069749 <1658343900@qq.com>
2019-07-17 09:14:57 +0800

    person

---
+++ src/main/java/com/qf/j1902/vo/Person.java
@@ -0,0 +1,35 @@
+package com.qf.j1902.vo;
+
+/**
+ * Created by Administrator on 2019/7/17.
+ */
+public class Person {
+
+    private  Integer age;
+    private String name;
+    private  String address;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
